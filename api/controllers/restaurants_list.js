4f1e6d6a218c5bcd93b97ae08dcfe2b88cd11fe1
Yohanna Gadelrab <Yohanna@users.noreply.github.com>
2016-11-09 11:36:57 -0500

    Implement restaurants_list path

--- api/controllers/restaurants_list.js
+++ api/controllers/restaurants_list.js
@@ -3,14 +3,33 @@ var util = require('util');
 // module.export = getList;
 
 module.exports = {
-  getList: getList
+  getRestaurantsList: getRestaurantsList
 }
 
-function getList(req, res) {
+var dummyData =
+  [{
+    id: 55,
+    name: "Pizza Hut",
+    location_long: "2341",
+    location_lat: "12342",
+    distance: "5345",
+    cuisine_type: "Pizza"
+  },
+  {
+    id: 55,
+    name: "Pizza Hut",
+    location_long: "2341",
+    location_lat: "12342",
+    distance: "5345",
+    cuisine_type: "Pizza"
+  }
+  ]
+function getRestaurantsList(req, res) {
   // variables defined in the Swagger document can be referenced using req.swagger.params.{parameter_name}
   var name = req.swagger.params.name.value || 'stranger';
   var hello = util.format('Hello, %s!', name);
 
   // this sends back a JSON response which is a single string
-  res.json(hello);
+  // res.json(hello);
+  res.json(dummyData);
 }
