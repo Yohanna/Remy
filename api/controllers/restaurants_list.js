56fdbd184ec197d2a359ef635813409f2f975d0b
Yohanna Gadelrab <Yohanna@users.noreply.github.com>
2016-11-09 11:36:34 -0500

    Implement a restaurant_list controller

---
+++ api/controllers/restaurants_list.js
@@ -0,0 +1,16 @@
+var util = require('util');
+
+// module.export = getList;
+
+module.exports = {
+  getList: getList
+}
+
+function getList(req, res) {
+  // variables defined in the Swagger document can be referenced using req.swagger.params.{parameter_name}
+  var name = req.swagger.params.name.value || 'stranger';
+  var hello = util.format('Hello, %s!', name);
+
+  // this sends back a JSON response which is a single string
+  res.json(hello);
+}
