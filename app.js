8a96d8455fbd5d6a6dfea79470c4133c0436946e
Yohanna Gadelrab <Yohanna@users.noreply.github.com>
2016-11-24 01:49:52 -0500

    Replace 'var' with 'const' in app.js

--- app.js
+++ app.js
@@ -1,12 +1,12 @@
 'use strict';
 
-var SwaggerExpress = require('swagger-express-mw');
-var app = require('express')();
-var SwaggerUi = require('swagger-tools/middleware/swagger-ui');
+const SwaggerExpress = require('swagger-express-mw');
+const app = require('express')();
+const SwaggerUi = require('swagger-tools/middleware/swagger-ui');
 
 module.exports = app; // for testing
 
-var config = {
+const config = {
   appRoot: __dirname // required config
 };
 
@@ -19,7 +19,7 @@ SwaggerExpress.create(config, function(err, swaggerExpress) {
   // install middleware
   swaggerExpress.register(app);
 
-  var port = process.env.PORT || 10010;
+  const port = process.env.PORT || 10010;
   app.listen(port);
 
   if (swaggerExpress.runner.swagger.paths['/hello']) {
