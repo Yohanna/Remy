81dcb36b04f5e298ee96119379a959d1027c72b1
Owen Westland <Owen Westland>
2016-11-30 15:01:29 -0500

    Initial experimentation.

--- api/controllers/restaurants_list.js
+++ api/controllers/restaurants_list.js
@@ -1,4 +1,5 @@
 var util = require('util');
+var ranker = require('../../server/ranker.js');
 
 // module.export = getList;
 
@@ -6,30 +7,6 @@ module.exports = {
   getRestaurantsList: getRestaurantsList
 }
 
-var dummyData =
-  [{
-    id: 55,
-    name: "Pizza Hut",
-    location_long: "2341",
-    location_lat: "12342",
-    distance: "5345",
-    cuisine_type: "Pizza"
-  },
-  {
-    id: 55,
-    name: "Pizza Hut",
-    location_long: "2341",
-    location_lat: "12342",
-    distance: "5345",
-    cuisine_type: "Pizza"
-  }
-  ]
 function getRestaurantsList(req, res) {
-  // variables defined in the Swagger document can be referenced using req.swagger.params.{parameter_name}
-  var name = req.swagger.params.name.value || 'stranger';
-  var hello = util.format('Hello, %s!', name);
-
-  // this sends back a JSON response which is a single string
-  // res.json(hello);
-  res.json(dummyData);
+    res.json(ranker.rank(req));
 }
