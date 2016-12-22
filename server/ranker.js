03aa65d37513a00b0998ce0e92b0fcb1311a9887
Yohanna Gadelrab <Yohanna@users.noreply.github.com>
2016-12-21 20:49:16 -0500

    Rerturn an array of restaurants

--- server/ranker.js
+++ server/ranker.js
@@ -1,39 +1,58 @@
 'use strict';
-const config = require('../config/config.js');
+const config = require('../config/config');
 
-var ret = {};
+let ret = {};
 
-var googleMapClient = require('@google/maps').createClient({
-    // key: config.myKey.private_key
+let googleMapClient = require('@google/maps').createClient({
     key: config.API_KEY
 });
 
-var requestParameters = {};
+let requestParameters = {};
 
 function rank(req) {
-    var fid = 0;
-    var fname = 'The Test Place';
-    var flat = 10.01;
-    var flng = -10.2;
+    let API_list;
+    let ret = [];
+    let item = {
+       id: 'f',
+       name:'f',
+       lat: 25,
+       lng: 25
+    };
+
+    ret.push(item);
+    ret.push(item);
+    
+    // return ret;
 
     requestParameters.location = [43.571048, -79.767054];
-    requestParameters.radius = 50;
+    requestParameters.radius = 1000;
+    requestParameters.type = 'food';
+
+    // for(let i = 0; i < 2; i++){
+    //     ret.push({id:'found', name:'found', lat:50, lng:50});
+    //     //ret[i].id = 'problem';//API_list[i].id;
+    //     //ret[i].name = 'problem';//API_list[i].name;
+    //     //ret[i].lat = 50;//API_list[i].geometry.location.lat;
+    //     //ret[i].lng = 50;//API_list[i].geometry.location.lng;
+    // }
 
     try {
         googleMapClient.places(requestParameters, function (err, response) {
             if (err) { throw err; } // It'll be caught by the catch statement
+            
+            API_list = response.results;
+            if(API_list.length === 0){
+                console.log('No locations were returned.');
+                return 'No locations were found for the provided search.';
+            }
 
-            console.log('No Errors');
         });
-        
+         
     } catch (error) {
-        console.log(`Cought an error: ${error}`);
+        console.log(`Caught an error: ${error}`);
+        return 'There was an error with the API call.';
     }
-
-    ret.id = fid;
-    ret.name = fname;
-    ret.lat = flat;
-    ret.lng = flng;
+    console.log('No Errors');
 
     return ret;
 }
