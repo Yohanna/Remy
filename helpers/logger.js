55687983cf43c300308176df1efef979a118af02
Yohanna Gadelrab <Yohanna@users.noreply.github.com>
2016-12-23 00:55:12 -0500

    Add a helper files for winston logging options

---
+++ helpers/logger.js
@@ -0,0 +1,11 @@
+const winston = require('winston');
+
+const logger = module.exports = new (winston.Logger)({
+  transports: [
+    new (winston.transports.Console)({
+      colorize: 'all'
+    })
+  ]
+});
+
+module.exports = logger;
