13651ef69997c106a8e7512e364cbe6e23a7f468
Yohanna Gadelrab <Yohanna@users.noreply.github.com>
2016-12-25 02:54:24 -0500

    Add a test template for ranker.js

---
+++ test/server/ranker.js
@@ -0,0 +1,12 @@
+const should = require('should');
+const rank = require('../../server/ranker');
+
+describe('ranker', function () {
+
+  describe('#rank', function () {
+
+    it('should return a list of restaurants', function () {
+      // TODO write tests for ranker.rank
+    });
+  });
+});
