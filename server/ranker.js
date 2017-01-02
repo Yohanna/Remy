ef3bb51916f54b5e5e1bcebf4df985af0cc853e1
owenwestland <13orw@queensu.ca>
2017-01-01 22:28:51 -0500

    List is being populated, but not properly returned.

--- server/ranker.js
+++ server/ranker.js
@@ -1,8 +1,6 @@
 'use strict';
 const config = require('../config/config');
 
-let ret = {};
-
 let googleMapClient = require('@google/maps').createClient({
     key: config.API_KEY
 });
@@ -10,51 +8,30 @@ let googleMapClient = require('@google/maps').createClient({
 let requestParameters = {};
 
 function rank(req) {
-    let API_list;
+    let API_list = [];
+    let i;
     let ret = [];
-    let item = {
-       id: 'f',
-       name:'f',
-       lat: 25,
-       lng: 25
-    };
 
-    ret.push(item);
-    ret.push(item);
-    
-    // return ret;
-
-    requestParameters.location = [43.571048, -79.767054];
-    requestParameters.radius = 1000;
-    requestParameters.type = 'food';
-
-    // for(let i = 0; i < 2; i++){
-    //     ret.push({id:'found', name:'found', lat:50, lng:50});
-    //     //ret[i].id = 'problem';//API_list[i].id;
-    //     //ret[i].name = 'problem';//API_list[i].name;
-    //     //ret[i].lat = 50;//API_list[i].geometry.location.lat;
-    //     //ret[i].lng = 50;//API_list[i].geometry.location.lng;
-    // }
+    requestParameters.location = [req.swagger.params.location_lat.value, req.swagger.params.location_long.value];
+    requestParameters.radius = req.swagger.params.distance.value;
+    requestParameters.type = "restaurant";
+    requestParameters.query = "";
 
     try {
         googleMapClient.places(requestParameters, function (err, response) {
-            if (err) { throw err; } // It'll be caught by the catch statement
-            
-            API_list = response.results;
-            if(API_list.length === 0){
-                console.log('No locations were returned.');
-                return 'No locations were found for the provided search.';
-            }
+            if(!err){
+                API_list = response.json.results;
 
+                for(i = 0; i < API_list.length; i++){
+                    ret.push({"id":API_list[i].id, "name":API_list[i].name});
+                }
+            }
+            return ret;
         });
          
     } catch (error) {
-        console.log(`Caught an error: ${error}`);
-        return 'There was an error with the API call.';
+        return `Caught an error: ${error}`;
     }
-    console.log('No Errors');
-
-    return ret;
 }
 
 module.exports.rank = rank;
