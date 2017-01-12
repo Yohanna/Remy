13be7e50951bf0c11dde1c950805a6b80d53840b
Yohanna Gadelrab <Yohanna@users.noreply.github.com>
2017-01-12 01:31:25 -0500

    Inform the user when their search didn't return any results

--- server/ranker.js
+++ server/ranker.js
@@ -1,5 +1,6 @@
 'use strict';
 const config = require('../config/config');
+const logger = require('../helpers/logger');
 
 const googleMapClient = require('@google/maps').createClient({
     key: process.env.GOOGLE_MAPS_API_KEY || config.API_KEY
@@ -9,12 +10,12 @@ function rank(req) {
     return new Promise(function (resolve, reject) {
         let API_list = [];
 
-        let user_location = {lat: req.swagger.params.location_lat.value, lng: req.swagger.params.location_long.value};
+        let user_location = { lat: req.swagger.params.location_lat.value, lng: req.swagger.params.location_long.value };
 
         let query = {
-            location: {lat: 44.2261, lng: -76.4966},
+            location: { lat: 44.2261, lng: -76.4966 },
             radius: req.swagger.params.distance.value,
-            keyword: req.swagger.params.cuisine_type.value, 
+            keyword: req.swagger.params.cuisine_type.value,
             rankby: 'prominence',
             type: 'restaurant'
         };
@@ -23,12 +24,16 @@ function rank(req) {
             if (!err) {
                 API_list = response.json.results;
 
-                let ret = [];
+                if (API_list.length === 0) {
+                    logger.info('No locations were found for the provided search.');
+                    reject('No locations were found for the provided search.');
+                }
+
+                let ret = []
                 for (let i = 0; i < API_list.length; i++) {
                     ret.push({ 'id': API_list[i].id, 'name': API_list[i].name });
                 }
 
-                // Return ret
                 resolve(response);
             } else {
                 reject(err);
