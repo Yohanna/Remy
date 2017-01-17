92e80de4e5f1dd692f2c2bec9b7500eaf0f539cb
Yohanna Gadelrab <Yohanna@users.noreply.github.com>
2017-01-16 20:56:10 -0500

    Start a test for ranker.rank()

--- test/server/ranker.js
+++ test/server/ranker.js
@@ -1,12 +1,32 @@
 const should = require('should');
-const rank = require('../../server/ranker');
+const ranker = require('../../server/ranker');
 
 describe('ranker', function () {
 
   describe('#rank', function () {
 
     it('should return a list of restaurants', function () {
-      // TODO write tests for ranker.rank
+      let params = {
+        location: { lat: 44.2261, lng: -76.4966 },
+        radius: 5000,
+        keyword: 'pizza',
+        opennow: false,
+        rankby: 'prominence',
+        type: 'restaurant'
+
+      };
+
+      ranker.rank(params)
+        .then(function (list) {
+
+        });
+    });
+
+    it('should not find any restaurants', function () {
+      let params = {
+
+      };
+
     });
   });
 });
