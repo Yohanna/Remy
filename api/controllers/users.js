dcac5016eba00d8967b860110a00c01c11484c40
Yohanna Gadelrab <Yohanna@users.noreply.github.com>
2017-01-22 23:20:52 -0500

    Finish addUser & updateUser

--- api/controllers/users.js
+++ api/controllers/users.js
@@ -5,27 +5,41 @@ const db = require('../../db/db');
 
 
 function getUser(req, res) {
+  const userID = req.swagger.params.id.value;
 
-  const user_id = req.swagger.params.id.value;
-
-  db.getUser(user_id)
+  db.getUser(userID)
     .then(function (result) {
-      console.log(result);
       res.send(result);
     })
     .catch(function (reason) {
-      console.log(reason);
-      res.send(reason);
+      // res.send(reason);
+      res.status(404).send(reason);
     });
 }
 
 function addUser(req, res) {
-  res.send('add user');
-  console.log(req.swagger.params.User.value);
+  const newUser = req.swagger.params.user.value;
+
+  db.addUser(newUser)
+    .then(() => {
+      res.sendStatus(200);
+    })
+    .catch((reason) => {
+      res.send(reason);
+    });
 }
 
 function updateUser(req, res) {
-  res.send('update user');
+  const userID = req.swagger.params.id.value;
+  const newUser = req.swagger.params.user.value;
+
+  db.updateUser(userID, newUser)
+    .then(() => {
+      res.sendStatus(200);
+    })
+    .catch((reason) => {
+      res.send(reason);
+    });
 }
 
 function deleteUser(req, res) {
