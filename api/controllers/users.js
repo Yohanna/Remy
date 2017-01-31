e8e0d32349897b16a91444cf242e1e826fbf6dcb
Yohanna Gadelrab <Yohanna@users.noreply.github.com>
2017-01-30 20:36:02 -0500

    Temporarily send a dummy response for UserMetrics

--- api/controllers/users.js
+++ api/controllers/users.js
@@ -14,6 +14,10 @@ function getUser(req, res) {
     });
 }
 
+function getUserMetrics(req, res) {
+  res.send('Metrics');
+}
+
 function getAllUsers(req, res) {
   db.getAllUsers()
     .then(function (result) {
@@ -65,6 +69,7 @@ function deleteUser(req, res) {
 module.exports = {
   getUser: getUser,
   getAllUsers: getAllUsers,
+  getUserMetrics: getUserMetrics,
   addUser: addUser,
   updateUser: updateUser,
   deleteUser: deleteUser
