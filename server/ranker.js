86a48e82596261b25893c832e4be40fa83118234
Yohanna Gadelrab <Yohanna@users.noreply.github.com>
2017-02-15 18:55:25 -0500

    Take User Metrics into account when returning the restaurants list [closes #7]

--- server/ranker.js
+++ server/ranker.js
@@ -1,6 +1,7 @@
 'use strict';
 const config = require('../config/config');
 const logger = require('../helpers/logger');
+const db = require('../db/db');
 
 const googleMapClient = require('@google/maps').createClient({
     key: config.API_KEY
@@ -8,39 +9,121 @@ const googleMapClient = require('@google/maps').createClient({
 
 function rank(params) {
     return new Promise(function (resolve, reject) {
-        let API_list = [];
-
-        let user_location = { lat: params.location_lat.value, lng: params.location_long.value };
+        let apiList = [];
 
         // location: { lat: 44.227917, lng: -76.495611 },
-        let query = {
-            location: user_location,
-            radius: params.distance.value,
-            keyword: params.cuisine_type.value,
-            opennow: params.open_now.value,
-            type: 'restaurant'
-        };
+        const user_location = { lat: params.location_lat.value, lng: params.location_long.value };
+        const user_id = params.user_id.value;
 
-        googleMapClient.placesNearby(query, function (err, response) {
-            if (!err) {
-                API_list = response.json.results;
+        let query = {};
 
-                if (API_list.length === 0) {
-                    reject('No locations were found for the provided search.');
-                }
+        if (params.cuisine_type.value === undefined) {
+            query = {
+                location: user_location,
+                radius: params.distance.value,
+                opennow: params.open_now.value,
+                type: 'restaurant'
+            };
+        } else {
+            query = {
+                location: user_location,
+                radius: params.distance.value,
+                keyword: params.cuisine_type.value,
+                opennow: params.open_now.value,
+                type: 'restaurant'
+            };
+        }
 
-                // let ret = []
-                // for (let i = 0; i < API_list.length; i++) {
-                //     ret.push({ 'id': API_list[i].id, 'name': API_list[i].name });
-                // }
-
-                resolve(API_list.slice(0, params.restaurants_count.value || 10));
-            } else {
+        googleMapClient.placesNearby(query, (err, response) => {
+            if (err) {
                 logger.error('Google Maps API called failed');
                 reject(err);
+
+            } else if (response.json.results.length === 0) {
+                reject('No locations were found for the provided search.');
+            } else {
+                apiList = response.json.results;
+
+                // If the user did Not supply a cuisine_type but we know the user (we have a user_id),
+                // then use the user's preferences from the DB
+                if (params.cuisine_type.value === undefined && user_id !== undefined) {
+                    // 1. Get User's data from DB
+                    // 2. Make another request to Google to get a list with same query but include the user's keywords in it
+                    // 3. Return the new list and combine with the old one
+                    // 4. Sort as usual
+
+                    getCustomList(user_id, query)
+                        .then((newList) => {
+                            apiList = apiList.concat(newList);
+
+                            // Sort the list descendingly
+                            apiList.sort(compareRatings);
+
+                            // Remove duplicate items
+                            apiList = removeDuplicates(apiList);
+
+                            // Return the first restaurants_count from the list or 10 if no count is provided
+                            resolve(apiList.slice(0, params.restaurants_count.value || 10));
+                        })
+                        .catch((reason) => {
+                            return reject(reason);
+                        });
+                } else {
+                    // Sort the list descendingly
+                    apiList.sort(compareRatings);
+
+                    // Return the first restaurants_count from the list or 10 if no count is provided
+                    resolve(apiList.slice(0, params.restaurants_count.value || 10));
+
+                }
             }
         });
     });
+}
+
+
+function compareRatings(x, y) {
+
+    // If no rating property, consider y to be smaller than x, i.e. move the item down the list
+    if (x.rating === undefined || y.rating === undefined)
+        return 1;
+
+    // Sort descendingly
+    return (y.rating - x.rating);
+}
+
+function getCustomList(user_id, query) {
+    return new Promise((resolve, reject) => {
+        db.getUserMetrics(user_id)
+            .then((userMetrics) => {
+
+                // Add the user's preferences to the query
+                query.keyword = userMetrics.favorite_food[0];
+
+                googleMapClient.placesNearby(query, (err, response) => {
+                    if (err) {
+                        logger.error('Google Maps API called failed');
+                        return reject(err);
+                    }
+
+                    return resolve(response.json.results);
+                });
+            })
+            .catch((reason) => {
+                logger.error(reason);
+            });
+    });
+}
+
+function removeDuplicates(arr) {
+
+    for (let i = 0; i < arr.length - 1; i++) {
+        if (arr[i].place_id === arr[i + 1].place_id) {
+            arr.splice(i, 1);
+        }
+    }
+
+    return arr;
 }
 
 module.exports.rank = rank;
