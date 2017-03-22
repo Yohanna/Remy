ea48be6838f16bac154370e4334323d02ec3be38
Yohanna Gadelrab <Yohanna@users.noreply.github.com>
2017-03-22 18:01:46 -0400

    Add a controller template for handling recent searches API

---
+++ api/controllers/recent_search.js
@@ -0,0 +1,17 @@
+'use strict';
+const db = require('../../db/db');
+
+
+function getRecentSearch(req, res) {
+  res.sendStatus(200);
+}
+
+
+function addRecentSearch(req, res) {
+  res.sendStatus(200);
+}
+
+module.exports = {
+  getRecentSearch,
+  addRecentSearch
+};
