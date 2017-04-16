62b0f46678da124ce4a06f4e2b8ccb684d7ee263
Yohanna Gadelrab <Yohanna@users.noreply.github.com>
2017-04-16 04:32:33 -0400

    test(controllers): finish testing hello_world controller

---
+++ test/api/controllers/hello_world.spec.ts
@@ -0,0 +1,40 @@
+import { Response } from "express";
+import * as chai from 'chai';
+import * as mocha from 'mocha';
+import * as supertest from 'supertest';
+
+const api = supertest('http://localhost:10010');
+const expect = chai.expect;
+
+describe('#hello_world', function () {
+  describe('GET /hello', function () {
+    it('respond with 200 Success', function (done) {
+      api.get('/hello')
+        .query({ name: 'World!' })
+        .set('Content-Type', 'application/json')
+        .expect(200)
+        .end(function (err, res) {
+          err ? done(err) : done();
+        });
+    });
+
+    it('respond with query sent', function (done) {
+      api.get('/hello')
+        .query({ name: 'World' })
+        .set('Content-Type', 'application/json')
+        .expect('"Hello, World!"')
+        .end(function (err, res) {
+          err ? done(err) : done();
+        });
+    });
+
+    it('respond with no query', (done) => {
+      api.get('/hello')
+        .set('Content-Type', 'application/json')
+        .expect('"Hello, stranger!"')
+        .end(function (err, res) {
+          err ? done(err) : done();
+        });
+    })
+  });
+});
