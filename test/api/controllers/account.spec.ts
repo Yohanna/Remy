1ba026f73b92f5b7d72b16c0659cb338ccfbee1c
Yohanna Gadelrab <Yohanna@users.noreply.github.com>
2017-04-17 12:05:08 -0400

    test(api/controllers): finish implementing account specs

--- test/api/controllers/account.spec.ts
+++ test/api/controllers/account.spec.ts
@@ -1,51 +1,8 @@
 import * as chai from 'chai';
 import * as supertest from 'supertest';
-var ZSchema = require('z-schema');
-var customFormats = module.exports = function (zSchema) {
-  // Placeholder file for all custom-formats in known to swagger.json
-  // as found on
-  // https://github.com/OAI/OpenAPI-Specification/blob/master/versions/2.0.md#dataTypeFormat
+const ZSchema = require('z-schema');
 
-  var decimalPattern = /^\d{0,8}.?\d{0,4}[0]+$/;
-
-  /** Validates floating point as decimal / money (i.e: 12345678.123400..) */
-  zSchema.registerFormat('double', function (val) {
-    return !decimalPattern.test(val.toString());
-  });
-
-  /** Validates value is a 32bit integer */
-  zSchema.registerFormat('int32', function (val) {
-    // the 32bit shift (>>) truncates any bits beyond max of 32
-    return Number.isInteger(val) && ((val >> 0) === val);
-  });
-
-  zSchema.registerFormat('int64', function (val) {
-    return Number.isInteger(val);
-  });
-
-  zSchema.registerFormat('float', function (val) {
-    // should parse
-    return Number.isInteger(val);
-  });
-
-  zSchema.registerFormat('date', function (val) {
-    // should parse a a date
-    return !isNaN(Date.parse(val));
-  });
-
-  zSchema.registerFormat('dateTime', function (val) {
-    return !isNaN(Date.parse(val));
-  });
-
-  zSchema.registerFormat('password', function (val) {
-    // should parse as a string
-    return typeof val === 'string';
-  });
-};
-
-customFormats(ZSchema);
-
-var validator = new ZSchema({});
+const validator = new ZSchema({});
 const api = supertest('http://localhost:10010'); // supertest init;
 const expect = chai.expect;
 
@@ -64,7 +21,10 @@ describe('#account', function () {
 
       api.post('/account/login')
         .set('Content-Type', 'application/json')
-        .send({ LoginInfo: 'DATA GOES HERE' })
+        .send({
+          email: "yohanna@email.com",
+          password: "1234"
+        })
         .expect(200)
         .end(function (err, res) {
           if (err) return done(err);
@@ -89,13 +49,15 @@ describe('#account', function () {
       api.post('/account/login')
         .set('Content-Type', 'application/json')
         .send({
-          LoginInfo: 'DATA GOES HERE'
+          email: "doesnotexist@email.com",
+          password: "1234"
         })
         .expect(403)
         .end(function (err, res) {
           if (err) return done(err);
 
           expect(validator.validate(res.body, schema)).to.be.true;
+          expect(res.body.message).to.be.equal('Incorrect login info');
           done();
         });
     });
@@ -115,13 +77,13 @@ describe('#account', function () {
       api.post('/account/login')
         .set('Content-Type', 'application/json')
         .send({
-          LoginInfo: 'DATA GOES HERE'
+          LoginInfo: 'Wrong Format'
         })
-        .expect('DEFAULT RESPONSE CODE HERE')
         .end(function (err, res) {
           if (err) return done(err);
 
           expect(validator.validate(res.body, schema)).to.be.true;
+          expect(res.body.message).to.be.equal('Incorrect login info');
           done();
         });
     });
